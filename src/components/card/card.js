18fde52da21c4ddff5fdea59af0370f3fd2eaacd
AJ Kumar <ajanthan.uthayakumar@accenture.com>
2020-06-02 02:27:02 +0100

    added card

---
+++ src/components/card/card.js
@@ -0,0 +1,61 @@
+import React from 'react'
+import './card.css'
+
+export default function Card() {
+    return (
+        <div className="cardContainer">
+            <div className="card">
+                <div class="imgBx" data-text="Design">
+                    <p>Text 1</p>
+                </div>
+                <div class="cardContent">
+                    <div>
+                        <h3>Design</h3>
+                        <p>Qui reprehenderit nisi nulla elit mollit incididunt cupidatat consequat in.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div class="imgBx" data-text="Code">
+                    <p>Text 2</p>
+                </div>
+                <div class="cardContent">
+                    <div>
+                        <h3>Code</h3>
+                        <p>Qui reprehenderit nisi nulla elit mollit incididunt cupidatat consequat in.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div class="imgBx" data-text="Launch">
+                    <p>Text 3</p>
+                </div>
+                <div class="cardContent">
+                    <div>
+                        <h3>Launch</h3>
+                        <p>Qui reprehenderit nisi nulla elit mollit incididunt cupidatat consequat in.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+            <div className="card">
+                <div class="imgBx" data-text="Earn">
+                    <p>Text 4</p>
+                </div>
+                <div class="cardContent">
+                    <div>
+                        <h3>Earn</h3>
+                        <p>Qui reprehenderit nisi nulla elit mollit incididunt cupidatat consequat in.</p>
+                        <a href="#">Read more</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
