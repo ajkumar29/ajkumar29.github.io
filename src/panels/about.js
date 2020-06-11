607a61c535cc36ea1cc34291fab4c1613255d1c5
AJ Kumar <ajanthan.uthayakumar@accenture.com>
2020-06-11 23:58:17 +0100

    added text to about.js

--- src/panels/about.js
+++ src/panels/about.js
@@ -4,8 +4,19 @@ import "./about.css";
 export default function About() {
   return (
     <div className="aboutContainer">
-      <div data-aos="fade-up" className="abouttext1">
-        Hello world
+      <div data-aos="fade-up">
+        I am self-motivated, hardworking and passionate about technology. I have
+        all-round technical skills combined with good practical knowledge of
+        analysing and solving problems using programming. I enjoy honing my
+        programming skillset by solving challenges and developing applications
+        in my spare time. I use my questioning skills to analyse and break down
+        complex problems to implement efficient solutions. I am comfortable
+        working in a dynamic, team-based environment and where necessary, I can
+        also deliver results working independently.
+      </div>
+      <div data-aos="fade-up">
+        As a full stack developer, I have experience with both Front-end and
+        backend projects.
       </div>
     </div>
   );
